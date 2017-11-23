222193392eed4e015e9cb61720ca090309827ca1
wangyingbo <wangyingbo@winupon.net>
2017-11-23 18:29:09 +0800

    详见各部分readme

---
+++ chapter1/src/main/java/com/example/sbdemo/SbdemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.sbdemo;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdemoApplication {
+
+	public static void main(String[] args) {
+		/**
+		 * 隐藏banner启动方式
+		 */
+		SpringApplication springApplication = new SpringApplication(SbdemoApplication.class);
+		//设置banner的模式为隐藏
+		springApplication.setBannerMode(Banner.Mode.OFF);
+
+		SpringApplication.run(SbdemoApplication.class, args);
+	}
+}
---
+++ chapter3/README.md
@@ -0,0 +1,1 @@
+## 6.SpringBoot整合JavaMail发送邮件
