a059849d3013aec208ae05d94c805d3cf0976d05
wangyingbo <wangyingbo@winupon.net>
2017-11-24 18:14:13 +0800

    swagger

--- chapter1/src/main/java/com/example/sbdemo/controller/UserController.java
+++ chapter1/src/main/java/com/example/sbdemo/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.example.sbdemo.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping(value = "/login")
+    @ApiOperation(value="删除图书", notes="根据url的id来指定删除图书")
+    @RequestMapping(value="login", method= RequestMethod.GET)
     public String login(){
         return "Hello World";
     }
