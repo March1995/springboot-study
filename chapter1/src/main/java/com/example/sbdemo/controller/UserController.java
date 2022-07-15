473145be5f3c4a57b1e100bc56bdbdffe4ad0c1d
Marcher <1501987783@qq.com>
2022-07-16 00:23:29 +0800

    统一依赖管理

--- chapter1/src/main/java/com/example/sbdemo/controller/UserController.java
+++ chapter1/src/main/java/com/example/sbdemo/controller/UserController.java
@@ -1,29 +1,28 @@
 package com.example.sbdemo.controller;
 
 import com.example.chapter5.property.MyPropertyService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+/**
+ * @author Marcher丶
+ */
 @RestController
 public class UserController {
 
     @Resource
     private MyPropertyService propertyService;
 
-    @ApiOperation(value="删除图书", notes="根据url的id来指定删除图书")
-    @RequestMapping(value="login", method= RequestMethod.GET)
-    public String login(){
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String login() {
         return "Hello World";
     }
 
-    @RequestMapping(value="test", method= RequestMethod.GET)
-    private String testService(){
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    private String testService() {
         return propertyService.getInfo();
     }
 }
